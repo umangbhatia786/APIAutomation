7212002474272b41379e8d31f82afa5ed9bec817
Umang Bhatia <umangbhatia786@gmail.com>
2020-12-26 15:46:03 +0530

    Added feature of running tag specific hook

--- features/environment.py
+++ features/environment.py
@@ -5,9 +5,11 @@ from utilities.resources import ApiResources
 
 
 def after_scenario(context, scenario):
-    context.delete_book_payload = requests.post(
-        url=f'{context.my_config["API"]["endpoint"]}{ApiResources.delete_book}',
-        json=delete_book_payload(context.book_id),
-        headers={'Content-Type': 'application/json'}
-    )
-    assert context.delete_book_payload.status_code == 200, 'Book Deletion failed'
+    """After Scenario Hook that only runs for regression tag"""
+    if 'regression' in scenario.tags:
+        context.delete_book_payload = requests.post(
+            url=f'{context.my_config["API"]["endpoint"]}{ApiResources.delete_book}',
+            json=delete_book_payload(context.book_id),
+            headers={'Content-Type': 'application/json'}
+        )
+        assert context.delete_book_payload.status_code == 200, 'Book Deletion failed'
